8b2a253570337f8302436700809a6fb0552d8b16
Manuel Klug <manuelalejandroklug@gmail.com>
2020-08-31 20:22:18 -0300

    cambios data

--- Python UNSAM/Clase 4/termometro.py
+++ Python UNSAM/Clase 4/termometro.py
@@ -1,4 +1,7 @@
 import random
+import numpy
+
+#% Ejercicio 4.11
 
 temperatura = 37.5
 mu = 0
@@ -13,3 +16,23 @@ print('El valor máximo es:', max(resultado))
 print('El valor mínimo es:', min(resultado))
 print('El promedio es:', sum(resultado)/n)
 print('La mediana es:', sorted(resultado)[(len(resultado)//2)])
+
+#% Ejercicio 4.13
+
+temperatura = 37.5
+mu = 0
+sigma = 0.2
+n = 999
+
+resultado = []
+for x in range(n):
+    resultado.append(temperatura + random.normalvariate(mu, sigma))
+
+print('El valor máximo es:', max(resultado))
+print('El valor mínimo es:', min(resultado))
+print('El promedio es:', sum(resultado)/n)
+print('La mediana es:', sorted(resultado)[(len(resultado)//2)])
+
+
+array = np.array(resultado)
+np.save('../Data/Temperaturas.npy', array)
